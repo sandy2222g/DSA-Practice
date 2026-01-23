7105fd73e3268b74ee2a6eeaee71796a11112f6d
sandy2222g <sandysanjay817@gmail.com>
2026-01-23 11:21:54 +0530

    Create lowest knight moves backtracking.py

---
+++ non-leetcode/lowest knight moves backtracking.py
@@ -0,0 +1,38 @@
+n=5
+dest=[4,4]
+solution=[[0]*n for _ in range(n)]
+m=float('inf')
+
+def knights_tour(x,y,count=0):
+    if [x,y]==dest:
+        solution[x][y]=1
+        global m
+        m=min(m,count)
+        print(solution)
+        return False
+    if count>m:
+        return False
+    if 0 <= x < n and 0 <= y < n and solution[x][y]==0:
+        solution[x][y] = 1
+        if knights_tour(x+1,y+2,count+1):
+            return True
+        if knights_tour(x+1,y-2,count+1):
+            return True
+        if knights_tour(x-1,y+2,count+1):
+            return True
+        if knights_tour(x-1,y-2,count+1):
+            return True
+        if knights_tour(x+2,y+1,count+1):
+            return True
+        if knights_tour(x+2,y-1,count+1):
+            return True
+        if knights_tour(x-2,y+1,count+1):
+            return True
+        if knights_tour(x-2,y-1,count+1):
+            return True
+        solution[x][y] = 0
+    return False
+
+knights_tour(0,0)
+
+print(m)
