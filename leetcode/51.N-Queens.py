fcefdd00cd295abd70168ac277ccd7c6e0f287f3
sandy2222g <sandysanjay817@gmail.com>
2026-01-23 12:33:20 +0530

    Update 51.N-Queens.py

--- leetcode/51.N-Queens.py
+++ leetcode/51.N-Queens.py
@@ -1,3 +1,8 @@
+"""
+
+**single solution**
+
+
 n=4
 sol=[['.']*n for _ in range(n)]
 col=set()
@@ -27,4 +32,36 @@ def backtrack(r):
         d1.remove(r+c)
         d2.remove(r-c)
     return False
+backtrack(0)"""
+
+
+
+# all the possible path
+n=4
+sol=[['.']*n for _ in range(n)]
+col=set()
+d1=set()
+d2=set()
+
+def backtrack(r):
+    if r==n:
+        for i in range(n):
+            print("".join(sol[i]))
+        print("")
+        return
+    for c in range(n):
+        if c in col or r+c in d1 or r-c in d2:
+            continue
+        
+        sol[r][c]="Q"
+        col.add(c)
+        d1.add(r+c)
+        d2.add(r-c)
+        backtrack(r+1)
+
+
+        sol[r][c]='.'
+        col.remove(c)
+        d1.remove(r+c)
+        d2.remove(r-c)
 backtrack(0)
