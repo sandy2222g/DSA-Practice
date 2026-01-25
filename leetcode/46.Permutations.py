681af4c7a6ade92cc4f9aa98071a6be3ab45fbe7
sandy2222g <sandysanjay817@gmail.com>
2026-01-25 21:22:19 +0530

    Update 46.Permutations.py

---
+++ leetcode/46.Permutations.py
@@ -0,0 +1,16 @@
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        r=[]
+        def backtrack(c=[]):
+            if len(c)==len(nums):
+                r.append(c.copy())
+            for i in nums:
+                if i in c:
+                    continue
+                c.append(i)
+                backtrack(c)
+                c.pop()
+        backtrack()
+        return r
