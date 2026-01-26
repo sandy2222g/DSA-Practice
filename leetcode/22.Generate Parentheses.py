18c8cced0c6c43f0b62c9c0b33dd938fba693bae
sandy2222g <sandysanjay817@gmail.com>
2026-01-26 19:20:50 +0530

    Update 22.Generate Parentheses.py

--- leetcode/22.Generate Parentheses.py
+++ leetcode/22.Generate Parentheses.py
@@ -1,15 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
-	    def dfs(left, right, s):
-		    if len(s) == n * 2:
-			    res.append(s)
-                return 
-		    if left < n:
-			    dfs(left + 1, right, s + '(')
-		    if right < left:
-			    dfs(left, right + 1, s + ')')
-	    res = []
-	    dfs(0, 0, '')
-	    return res
-obj=Solution()
+        res = []
+
+        def dfs(left, right, s):
+            if len(s) == n * 2:
+                res.append(s)
+                return
+
+            if left < n:
+                dfs(left + 1, right, s + '(')
+
+            if right < left:
+                dfs(left, right + 1, s + ')')
+
+        dfs(0, 0, '')
+        return res
+
+
+obj = Solution()
 print(obj.generateParenthesis(3))
