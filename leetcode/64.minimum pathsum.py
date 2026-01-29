f7fbd26b84596c5e731988a547dffe1467e0bb45
sandy2222g <sandysanjay817@gmail.com>
2026-01-29 09:26:15 +0530

    Update 64.minimum pathsum.py

--- leetcode/64.minimum pathsum.py
+++ leetcode/64.minimum pathsum.py
@@ -1,3 +1,7 @@
+"""
+  **using Memoization **
+
+
 from functools import lru_cache
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
@@ -14,3 +18,35 @@ class Solution:
                 dfs(i-1,j),dfs(i,j-1)
             )
         return dfs(n-1,m-1)
+
+
+        
+        **Tabulation (2D DP array)**
+
+
+        class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        n=len(grid)
+        m=len(grid[0])
+        dp=[[float('inf')]*m for _ in range(n)]
+        dp[0][0]=grid[0][0]
+        for i in range(1,n):
+            dp[i][0]=dp[i-1][0] + grid[i][0]
+        for i in range(1,m):
+            dp[0][i]=dp[0][i-1]+grid[0][i]
+        for i in range(1,n):
+            for j in range(1,m):
+                dp[i][j]=min(dp[i-1][j],dp[i][j-1])+grid[i][j]
+        return(dp[n-1][m-1])
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        """
