d91a0dba34e604c428e95ed0e58822535d1c35d9
sandy2222g <sandysanjay817@gmail.com>
2026-02-12 21:09:45 +0530

    Update 135.Candy.py

---
+++ leetcode/135.Candy.py
@@ -0,0 +1,14 @@
+class Solution:
+    def candy(self, ratings: list[int]) -> int:
+        n=len(ratings)
+        dp=[1]*(n)
+        for i in range(1,n):
+            if ratings[i]>ratings[i-1]:
+                dp[i]=dp[i-1]+1
+            
+        for i in range(n-2,-1,-1):
+            if ratings[i]>ratings[i+1]:
+                dp[i]=max(dp[i],dp[i+1]+1)
+        return sum(dp)            
+obj=Solution()
+print(obj.candy([1,2,2]))
