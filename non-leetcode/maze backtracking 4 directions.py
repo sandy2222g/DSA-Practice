8ec1becea43e3e7b4dc24f5f88d66816ee4c5cd8
sandy2222g <sandysanjay817@gmail.com>
2026-02-24 22:08:20 +0530

    Update maze backtracking 4 directions.py

---
+++ non-leetcode/maze backtracking 4 directions.py
@@ -0,0 +1,19 @@
+n=int(input())
+r=[list(map(int,input().split())) for _ in range(n)]
+sol=[[0]*n for i in range(n)]
+def bt(x,y):
+    if x<0 or y<0 or x>=n or y>=n or sol[x][y]==1 or r[x][y]==1:
+        return 
+    if x==n-1 and y==n-1:
+        sol[x][y]=1
+        for i in sol:
+            print(*i)
+        exit()
+    sol[x][y]=1
+    bt(x+1,y)
+    bt(x,y+1)
+    bt(x-1,y)
+    bt(x,y-1)
+    sol[x][y]=0
+bt(0,0)
+print("No solution exists")
