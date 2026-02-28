5c1cbcd3d27202eae0ce26897e912b331de3be30
sandy2222g <sandysanjay817@gmail.com>
2026-02-28 16:24:50 +0530

    Create 3309.Maximum Possible Number by Binary Concatenation.py

---
+++ leetcode/3309.Maximum Possible Number by Binary Concatenation.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxGoodNumber(self, nums: List[int]) -> int:
+        s=[bin(i)[2:] for i in nums]
+        s.sort(reverse=True,key=lambda x:x*10)
+        print(s)
+        return int("".join(s),2)
+obj=Solution()
+print(obj.maxGoodNumber([1,2,3]))
