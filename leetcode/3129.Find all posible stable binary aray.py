0a3a5baf29b876d49aea7ee3b9738389fb512def
sandy2222g <sandysanjay817@gmail.com>
2026-03-09 22:42:02 +0530

    Update 3129.Find all posible stable binary aray.py

---
+++ leetcode/3129.Find all posible stable binary aray.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+        m=10**9+7
+        dp = [[[0,0] for _ in range(one+1)] for _ in range(zero+1)]
+        for i in range(1,min(zero,limit)+1):
+            dp[i][0][0]=1
+        for j in range(1,min(one,limit)+1):
+            dp[0][j][1]=1
+        for i in range(1,zero+1):
+            for j in range(1,one+1):
+                over0=dp[i-limit-1][j][1] if i-limit>=1 else 0
+                over1=dp[i][j-limit-1][0] if j-limit>=1 else 0
+                dp[i][j][0]=(dp[i-1][j][0]+dp[i-1][j][1]-over0)%m
+                dp[i][j][1]=(dp[i][j-1][0]+dp[i][j-1][1]-over1)%m
+        return (dp[zero][one][0]+dp[zero][one][1])%m
+obj=Solution()
+print(obj.numberOfStableArrays(72,74,53))
