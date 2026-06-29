4e523c8fd5cff988b322bcee72ef7a970a098842
sandy2222g <sandysanjay817@gmail.com>
2026-06-29 11:42:48 +0530

    corrected the file

--- leetcode/98.Validate Binary Search Tree.py
+++ leetcode/98.Validate Binary Search Tree.py
@@ -1,3 +1,9 @@
+from typing import Optional
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode],l=None) -> bool:
         def rr(root,m=float('inf'),mi=-float("inf")):
